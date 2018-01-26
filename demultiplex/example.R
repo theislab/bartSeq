13650c35167ebb00c4e6e08ba89bbffd319aa6f1
Philipp A <flying-sheep@web.de>
2018-01-26 13:20:52 +0100

    Prettified the code, didn’t test yet

--- demultiplex/example.R
+++ demultiplex/example.R
@@ -1,90 +1,101 @@
-suppressMessages({library(Biostrings)
-library(ShortRead)
-library(parallel)
-library(stringi)
-library(gplots)
-library(reshape)
-library(stringr)
-library(foreach)
-library(doMC)
-library(DESeq2)
-library(plyr)
-library(reshape2)
+suppressPackageStartupMessages({
+	library(Biostrings)
+	library(ShortRead)
+	library(parallel)
+	library(stringi)
+	library(gplots)
+	library(reshape)
+	library(stringr)
+	library(foreach)
+	library(doMC)
+	library(DESeq2)
+	library(plyr)
+	library(reshape2)
+	library(glue)
 })
-options(java.parameters = "-Xmx8000m")
-registerDoMC(cores=4)
 
-# parse results
-parseRes <- function (results, res.loc, run.id) {
-  count.dir <- paste0(res.loc, "/counts/")
-  if(!file.exists(count.dir)) {
-    dir.create(path = count.dir, recursive = T)
-  }
-  for (i in c(1:length(results))) {
-    res= results[[i]]
-    res$cell.id <- paste(res$left.bc, res$right.bc, sep="-")
-    m <- unlist(strsplit(names(results)[i], "_"))
-    lib.info <- read.delim(paste0(res.loc, "/sample_desc/", tolower(m[grep("lib*\\d+", m, ignore.case = T)]), "_info.txt"))
-    lib.info$cell.id <- paste0(lib.info$left, "-", lib.info$right)
-    
-    res2 <- res[(which(res$cell.id%in%lib.info$cell.id==T)), ] # filter out the combinations that do not exist in the library
-    res3 <- t(table(res2[, c(1, 4)]))
-    res3 <- data.frame(cbind(row.names(res3), res3))
-    names(res3)[1] <- "cell.id"
-    
-    res4 <- merge(lib.info, res3, by="cell.id", all.x=T)
-    
-    write.table(res4,file=paste0(count.dir,"/" ,names(results)[i], "_count_matrix_", run.id, ".txt"), sep="\t", col.names=T, row.names=F, quote=F)
-    
-    res2 <- res[(which(res$cell.id%in%lib.info$cell.id==F)), ] # filter out the combinations that do not exist in the library
-    res3 <- t(table(res2[, c(1, 4)]))
-    res3 <- data.frame(cbind(row.names(res3), res3))
-    names(res3)[1] <- "cell.id"
-    write.table(res3,file=paste0(count.dir,"/" ,names(results)[i], "_count_matrix_extrabc_", run.id, ".txt"), sep="\t", col.names=T, row.names=F, quote=F)
-  }
+here <- function() {
+	args <- commandArgs(trailingOnly = FALSE)
+	needle <- "--file="
+	match <- grep(needle, args)
+	file <-
+		if (length(match) > 0) # Rscript
+			sub(needle, "", args[match])
+		else # 'source'd via R console
+			sys.frames()[[1]]$ofile
+	normalizePath(dirname(file))
 }
+
+parse_results <- function(results, res_loc, run_id) {
+	count.dir <- glue("{res_loc}/counts")
+	dir.create(path = count.dir, recursive = TRUE, showWarnings = FALSE)
+	
+	for (r in names(results)) {
+		res <- results[[r]]
+		res$cell.id <- paste(res$left.bc, res$right.bc, sep = "-")
+		m <- unlist(strsplit(r, "_"))
+		m2 <- tolower(m[grep("lib*\\d+", m, ignore.case = TRUE)])
+		lib_info <- read.delim(glue("{res_loc}/sample_desc{m2}_info.txt"))
+		lib_info$cell.id <- glue_data(lib_info, "{left}-{right}")
+		
+		res2 <- res[which(res$cell.id %in% lib_info$cell.id), ] # filter out the combinations that do not exist in the library
+		res3 <- t(table(res2[, c(1, 4)]))
+		res3 <- data.frame(cbind(row.names(res3), res3))
+		names(res3)[1] <- "cell.id"
+		
+		res4 <- merge(lib_info, res3, by = "cell.id", all.x = TRUE)
+		
+		write.table(res4, file = glue("{count.dir}/{r}_count_matrix_{run_id}.txt"), sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+		
+		res2 <- res[which(!(res$cell.id %in% lib_info$cell.id)), ] # filter out the combinations that do not exist in the library
+		res3 <- t(table(res2[, c(1, 4)]))
+		res3 <- data.frame(cbind(row.names(res3), res3))
+		names(res3)[1] <- "cell.id"
+		write.table(res3, file = glue("{count.dir}/{r}_count_matrix_extrabc_{run_id}.txt"), sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+	}
+}
+
 # call the functions for processing
-callMapping <- function(lib.loc, res.loc, bc.splitter, bc.missmatch, run.blast, eval.cut, run.id){
- getQC(lib.loc, res.loc) 
- mergePairedEnds(lib.loc, res.loc)
- generateInserts (lib.loc, res.loc, run.blast)
- getBarcodeCombinations (lib.loc, res.loc)
- files <- blastReads (lib.loc, res.loc, bc.splitter, bc.missmatch, run.blast)
- files = list.files(paste0(res.loc, "processed_data"),pattern = "extendedFrags",full.names = T)
- # split basename with "."
- m=regexpr(".+/",files)
- # names the files with the representation
- names(files) = sapply(strsplit(substring(files,attr(m,"match.length")+1),".",fixed=T),function(x)x[1])
- results <- tabulateMappingResults (files, lib.loc, res.loc, eval.cut) 
- names(results) <- names(files)
- save(results, file=paste0(res.loc, "allres_", run.id, ".RData"))
- load(paste0(res.loc, "alllibs_res_", run.id, ".RData"))
- names(results) <- sub("-", "", sub("NGS\\d+-", "", names(results)))
- parseRes(results, res.loc, run.id)
+call_mapping <- function(lib_loc, res_loc, bc_splitter, bc_mismatch, run_blast, eval_cut, run_id, queue) {
+	get_qc(lib_loc, res_loc) 
+	merge_paired_ends(lib_loc, res_loc)
+	generate_inserts(res_loc, run_blast)
+	get_barcode_combinations(res_loc)
+	files <- blast_reads(res_loc, bc_splitter, bc_mismatch, run_blast, queue)
+	files <- list.files(glue("{res_loc}/processed_data"), pattern = "extendedFrags", full.names = TRUE)
+	# split basename with "."
+	m <- regexpr(".+/", files)
+	# names the files with the representation
+	names(files) <- sapply(strsplit(substring(files, attr(m, "match.length") + 1), ".", fixed = TRUE), function(x) x[1])
+	results <- tabulate_mapping_results(files, lib_loc, res_loc, eval_cut) 
+	names(results) <- names(files)
+	save(results, file = glue("{res_loc}/allres_{run_id}.RData"))
+	load(glue("{res_loc}/alllibs_res_{run_id}.RData"))
+	names(results) <- sub("-", "", sub("NGS\\d+-", "", names(results)))
+	parse_results(results, res_loc, run_id)
 }
+
+HERE <- here()
+
+options(java.parameters = "-Xmx8000m")
+registerDoMC(cores = 4)
 
 ### expected
-# res.loc/barcodes/barcodes.txt
-# res.loc/amplicons/amplicons.txt
-# res.loc/sample_desc/library info
-# raw data
-ngs.id <- "ngs21"
-
-# flags
-run.blast = T
-queue = T
-run.id="e35_bcmm0"
+# res_loc/barcodes/barcodes.txt
+# res_loc/amplicons/amplicons.txt
+# res_loc/sample_desc/library info
+# res_loc/raw_data
+res_loc <- "data/ngs21"
 
 # other scripts
-source("/src/fastq2count.R")
-bc.splitter <- "/src/barcode_splitter.pl"
-# parameters 
-bc.missmatch <- 0
-eval.cut <- 1e-35
-# data locations
-lib.loc <- paste0("/", ngs.id, "/rawdata/")
-res.loc <- paste0("/", ngs.id, "/")
-if(!file.exists(res.loc)) {
-  dir.create(path = res.loc, recursive = T)
-}
-callMapping (lib.loc, res.loc, bc.splitter, bc.missmatch, run.blast, eval.cut, run.id)
+source(glue("{HERE}/src/fastq2count.R"))
+
+call_mapping(
+	lib_loc = glue("{res_loc}/rawdata"),
+	res_loc = res_loc,
+	bc_splitter = "{HERE}/src/barcode_splitter.pl",
+	bc_mismatch = 0,
+	run_blast = TRUE,
+	eval_cut = 1e-35,
+	run_id = "e35_bcmm0",
+	queue = FALSE)
--- demultiplex/src/fastq2count.R
+++ demultiplex/src/fastq2count.R
@@ -1,129 +1,160 @@
 # author: richa batra 
 # modified original code of steffen saas
 # last modified: 20 Dec 2017
-library(Biostrings)
-library(ShortRead)
-library(parallel)
-library(stringi)
-library(gplots)
-library(reshape)
-library(stringr)
-library(foreach)
-library(doMC)
-library(DESeq2)
-library(plyr)
-library(reshape2)
+suppressPackageStartupMessages({
+	library(Biostrings)
+	library(ShortRead)
+	library(parallel)
+	library(stringi)
+	library(gplots)
+	library(reshape)
+	library(stringr)
+	library(foreach)
+	library(doMC)
+	library(DESeq2)
+	library(plyr)
+	library(reshape2)
+	library(glue)
+})
+
+shell_transformer <- function(code, envir) shQuote(glue::evaluate(code, envir))
 
 # quality control of the reads with FASTQC
-getQC <- function (lib.loc, res.loc) {
- qc.loc <- paste0(res.loc, "/qc/")
- if(!file.exists(qc.loc)) {
-  dir.create(path = qc.loc, recursive = T)
- }
- reads <- list.files(path=lib.loc, pattern=".gz", full.names=T)
- lapply(1:length(reads), FUN=function(i){
- i.read <- reads[i]
- sys.out <- paste0("/path/to/FASTQC/dir/fastqc ", i.read, " -o ", qc.loc)
- system(sys.out) 
- })
+get_qc <- function(lib_loc, res_loc) {
+	qc_loc <- glue("{res_loc}/qc")
+	dir.create(path = qc_loc, recursive = TRUE, showWarnings = FALSE)
+	
+	reads <- list.files(path = lib_loc, pattern = ".gz", full.names = TRUE)
+	lapply(seq_along(reads), FUN = function(i){
+		i.read <- reads[i]
+		system2("fastqc", c("fastqc", i.read, "-o", qc_loc)) 
+	})
 }
-# merging the overlapping paired reads using FLASH
-mergePairedEnds<- function (lib.loc, res.loc) {
- pdata.loc <- paste0(res.loc, "/processed_data/")
- if(!file.exists(pdata.loc)) {
-   dir.create(path = pdata.loc, recursive = T)
- }
- read1s <- list.files(path=lib.loc, pattern="R1.+gz", full.names=T)
- lapply(1:length(read1s), FUN=function(i){
-  reads_1.fq <- read1s[i]
-  reads_2.fq <- sub("R1", "R2", read1s[i])
-  if(file.exists(reads_1.fq) & file.exists(reads_2.fq)) {
-   sys.out <- paste0("/path/to/FLASH/dir/flash ", reads_1.fq, " ", reads_2.fq, " -M 150 -o", sub("_R1.*", "", basename(reads_1.fq)), " -d", pdata.loc)
-   system(sys.out)
-  }
- })
-}
-# formatting the reference amplicons to be used as blast reference database
-generateInserts <- function (lib.loc, res.loc, run.blast) {
- am.loc <- paste0(res.loc, "/amplicons/")
- sample.desc <- read.delim(paste0(am.loc, "amplicons.txt"))
- fasta <- unlist(lapply(1:nrow(sample.desc), FUN=function(i) {
-    c(paste0(">",sample.desc$Gene[i]),
-      toupper(sample.desc$Amplicon[i]))}))
- write(fasta, sep="\n", file=paste0(am.loc, "inserts.txt"))
- inserts = readDNAStringSet(paste0(am.loc, "inserts.txt"))
- # blast to match each insert to human genes
- dir.create(path = paste0(res.loc, "blast"), recursive = T)
- if(run.blast)
- {
-   writeXStringSet(inserts, paste0(res.loc,"blast/inserts.fa"), width=10000)
-   cmd = paste0("/path/to/FORMATDB/dir/formatdb -p F -i ", paste0(res.loc, "blast/inserts.fa"), " -n ", paste0(res.loc, "blast/inserts"))
-   blast.out=try(system(cmd, intern = TRUE))
- }
-}
-# barcodes to be searched forward and reverse complementary
-getBarcodeCombinations <- function (lib.loc, res.loc) {
 
- bc.loc <- paste0(res.loc, "/barcodes/")
- bcs <- read.delim(paste0(bc.loc, "barcodes.txt"), header=T)
- names(bcs) <- c("ids", "seqs")
- write.table(data.frame(id=bcs$ids,seq=as.character(bcs$seqs)), file=paste0(bc.loc, "bctable_bol.txt"),sep="\t",quote=F,row.names=F,col.names=F)
- write.table(data.frame(id=bcs$ids,seq=as.character(reverseComplement(DNAStringSet(bcs$seqs)))), file=paste0(bc.loc, "bctable_eol.txt"), sep="\t",quote=F,row.names=F,col.names=F)
+# merging the overlapping paired reads using FLASH
+merge_paired_ends <- function(lib_loc, res_loc) {
+	pdata.loc <- glue("{res_loc}/processed_data")
+	dir.create(path = pdata.loc, recursive = TRUE, showWarnings = FALSE)
+	
+	read1s <- list.files(path = lib_loc, pattern = "R1.+gz", full.names = TRUE)
+	lapply(seq_along(read1s), FUN = function(i) {
+		reads_1.fq <- read1s[i]
+		reads_2.fq <- sub("R1", "R2", read1s[i])
+		if (file.exists(reads_1.fq) & file.exists(reads_2.fq)) {
+			system2("flash", paste(reads_1.fq, reads_2.fq, "-M", "150", "-o", sub("_R1.*", "", basename(reads_1.fq)), "-d", pdata.loc))
+		}
+	})
 }
+
+# formatting the reference amplicons to be used as blast reference database
+generate_inserts <- function(res_loc, run_blast) {
+	am_loc <- glue("{res_loc}/amplicons")
+	sample_desc <- read.delim(glue("{am_loc}/amplicons.txt"))
+	fasta <- unlist(lapply(seq_len(nrow(sample_desc)), FUN = function(i) {
+		c(paste0(">", sample_desc$Gene[i]),
+		toupper(sample_desc$Amplicon[i]))
+	}))
+	write(fasta, sep = "\n", file = glue("{am_loc}/inserts.txt"))
+	inserts <- readDNAStringSet(glue("{am_loc}/inserts.txt"))
+	# blast to match each insert to human genes
+	dir.create(path = glue("{res_loc}/blast"), recursive = TRUE)
+	if (run_blast) {
+		writeXStringSet(inserts, glue("{res_loc}/blast/inserts.fa"), width = 10000)
+		blast_out <- try(system2(
+			"formatdb", c(
+				"-p", "F",
+				"-i", glue("{res_loc}/blast/inserts.fa"),
+				"-n", glue("{res_loc}/blast/inserts")
+			), stdout = TRUE))
+	}
+}
+
+# barcodes to be searched forward and reverse complementary
+get_barcode_combinations <- function(res_loc) {
+	bc_loc <- glue("{res_loc}/barcodes")
+	bcs <- read.delim(glue("{bc_loc}/barcodes.txt"), header = TRUE)
+	names(bcs) <- c("ids", "seqs")
+	write.table(data.frame(id = bcs$ids, seq = as.character(bcs$seqs)), file = glue("{bc_loc}/bctable_bol.txt"), sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+	write.table(data.frame(id = bcs$ids, seq = as.character(reverseComplement(DNAStringSet(bcs$seqs)))), file = glue("{bc_loc}/bctable_eol.txt"), sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+}
+
 # blast the reads and in parallel perform barcode identification
-blastReads <- function(lib.loc, res.loc, bc.splitter, bc.missmatch, run.blast) {
-  # read in fastq
-  files = list.files(paste0(res.loc, "processed_data"),pattern = "extendedFrags",full.names = T)
-  # split basename with "."
-  m=regexpr(".+/",files)
-  # names the files with the representation
-  names(files) = sapply(strsplit(substring(files,attr(m,"match.length")+1),".",fixed=T),function(x)x[1])
-  # foreach file run blast
-  foreach (id = names(files)) %dopar%
-  {
-    cat(id,"\n")
-    if(run.blast)
-    {
-      reads.path = paste0(res.loc, "blast/", id, "_reads_sh.fa")
-      if (queue) cmd = paste0("echo \"cat ", files[id]," | awk '{ if (NR%4==1) print \\\">\\\"((NR-1)/4)+1 ; if (NR%4==2) print }' > ",reads.path,"; /path/to/BLAST/dir/blastall -p blastn -r 1 -G -1 -E -1 -m 8 -d ", paste0(res.loc,"blast/inserts")," -i ",reads.path," -o ", paste0(res.loc,"blast/",id,"_res_inserts.txt"),"\" | /path/to/QSUB/dir/qsub -o \".\" -e \".\" -q long@@core -hard -l job_mem=40G")
-      cat(cmd)
-      blast.out=try(system(cmd, intern = TRUE))
-    }
-    #barcode matching from the read
-    bc.loc <- paste0(res.loc, "/barcodes/")
-    if(!file.exists(paste0(bc.loc, "/bc_identification/"))) {
-      dir.create(path = paste0(bc.loc, "/bc_identification/"), recursive = T)
-    }
-    cat("running",id)
-    cmd = paste0("cat ", files[id], " | ", bc.splitter, " --partial 3 --eolbcfile ", bc.loc, "/bctable_eol.txt --bolbcfile ", bc.loc, "/bctable_bol.txt --mismatches ", bc.missmatch, " --o ", bc.loc, "/bc_identification/", id, ".txt")
-    bcident.out=try(system(cmd, intern = TRUE))
-  }
-  return(files)
+blast_reads <- function(res_loc, bc_splitter, bc_mismatch, run_blast, queue) {
+	# read in fastq
+	files <- list.files(glue("{res_loc}/processed_data"), pattern = "extendedFrags", full.names = TRUE)
+	# split basename with "."
+	m <- regexpr(".+/", files)
+	# names the files with the representation
+	names(files) <- sapply(strsplit(substring(files, attr(m, "match.length") + 1), ".", fixed = TRUE), function(x) x[1])
+	# foreach file run blast
+	foreach(id = names(files)) %dopar% {
+		cat(id, "\n")
+		if (run_blast) {
+			path_reads <- glue("{res_loc}/blast/{id}_reads_sh.fa")
+			path_inserts <- glue("{res_loc}/blast/inserts")
+			path_res_inserts <- glue("{res_loc}/blast/{id}_res_inserts.txt")
+			script <- glue(
+				"cat {files[id]} | awk '{ if (NR%4==1) print \">\"((NR-1)/4)+1; if (NR%4==2) print }' >{path_reads}",
+				"blastall -p blastn -r 1 -G -1 -E -1 -m 8 -d {path_inserts} -i {path_reads} -o {path_res_inserts}",
+				"", .sep = "\n", .transformer = shell_transformer)
+			cat(script)
+			
+			blast_out <- try(
+				if (queue) system2("qsub", c("-o", ".", "-e", ".", "-q", "long@@core", "-hard", "-l", "job_mem=40G"), input = script, stdout = TRUE)
+				else system2("bash", c("-c", script), stdout = TRUE))
+		}
+		#barcode matching from the read
+		bc_loc <- glue("{res_loc}/barcodes")
+		dir.create(path = glue("{bc_loc}/bc_identification"), recursive = TRUE, showWarnings = FALSE)
+		cat("running", id)
+		bc_ident_out <- try(system2(
+			bc_splitter, c(
+				"--partial", "3",
+				"--eolbcfile", glue("{bc_loc}/bctable_eol.txt"),
+				"--bolbcfile", glue("{bc_loc}/bctable_bol.txt"),
+				"--mismatches ", bc_mismatch,
+				"--o ", glue("{bc_loc}/bc_identification/{id}.txt")),
+			input = files[id], stdout = TRUE))
+	}
+	files
 }
+
 # formate the output into count matrix
-tabulateMappingResults <- function (files, lib.loc, res.loc, eval.cut) {
-  results <- lapply (names(files),function(id)
-  {
-    file <- files[id]
-    bc.ident <- read.delim(paste0(res.loc, "/barcodes/bc_identification/", id, ".txt"), stringsAsFactors = F, header=F, na.strings = "unmatched")
-    colnames(bc.ident) <- c("bol", "eol")
-    insertmatch <- read.table(paste0(res.loc, "/blast/", id, "_res_inserts.txt"),stringsAsFactors=F)
-    colnames(insertmatch) <- c("query","subject","percentage.id","alignment.length","mismatches","gap.openings","query.start","query.end","subject.start","subject.end","E.value","bit.score")
-    # filter the low quality matches
-    insertmatch <- insertmatch[insertmatch$E.value<eval.cut,]
-    insertmatch <- insertmatch[order(insertmatch$E.value),]
-    # if there are more than one match for a query
-    insertmatch <- insertmatch[which(!duplicated(insertmatch$query)),]
-    insertmatch <- insertmatch[order(insertmatch$query),]
-    # match to the barcodes given the query numbers are corresponding to the order of the barcodes
-    insertmatch$bol <- bc.ident$bol[insertmatch$query] # query hold the oder or reads in for blast and barcode identification  
-    insertmatch$eol <- bc.ident$eol[insertmatch$query] # and therefore can be used to map the two tables
-    res <- data.frame(amplicon=insertmatch$subject,left.bc=pmin(insertmatch$eol,insertmatch$bol),right.bc=pmax(insertmatch$eol,insertmatch$bol)) # left barcodes minimun in eol and right barcodes max in eol
-    # blast file has duplicates of reads and we 
-    rownames(res) <- insertmatch$query 
-    res <- res[grepl("L[[:digit:]]+",res$left.bc) & grepl("R[[:digit:]]+",res$right.bc),] # removing the nas
-   # list(fractions=c(frac.compl=nrow(res)/nrow(bc.ident),frac.insert=nrow(insertmatch) / nrow(bc.ident),frac.bol=sum(!is.na(bc.ident$bol))/nrow(bc.ident),frac.eol=sum(!is.na(bc.ident$eol))/nrow(bc.ident)),result.counts=table(res),result=res)
-  })
-  names(results) <- names(files)
-  return(results)
+tabulate_mapping_results <- function(files, lib_loc, res_loc, eval_cut) {
+	results <- lapply(names(files), function(id) {
+		file <- files[id]
+		bc_ident <- read.delim(glue("{res_loc}/barcodes/bc_identification/{id}.txt"), stringsAsFactors = FALSE, header = FALSE, na.strings = "unmatched")
+		colnames(bc_ident) <- c("bol", "eol")
+		insertmatch <- read.table(glue("{res_loc}/blast/{id}_res_inserts.txt"), stringsAsFactors = FALSE)
+		colnames(insertmatch) <- c(
+			"query", "subject", "percentage.id", "alignment.length", "mismatches", "gap.openings",
+			"query.start", "query.end", "subject.start", "subject.end", "E.value", "bit.score")
+		# filter the low quality matches
+		insertmatch <- insertmatch[insertmatch$E.value < eval_cut, ]
+		insertmatch <- insertmatch[order(insertmatch$E.value), ]
+		# if there are more than one match for a query
+		insertmatch <- insertmatch[which(!duplicated(insertmatch$query)), ]
+		insertmatch <- insertmatch[order(insertmatch$query), ]
+		# match to the barcodes given the query numbers are corresponding to the order of the barcodes
+		insertmatch$bol <- bc_ident$bol[insertmatch$query] # query hold the oder or reads in for blast and barcode identification  
+		insertmatch$eol <- bc_ident$eol[insertmatch$query] # and therefore can be used to map the two tables
+		res <- data.frame(
+			amplicon = insertmatch$subject,
+			left.bc = pmin(insertmatch$eol,insertmatch$bol),
+			right.bc = pmax(insertmatch$eol,insertmatch$bol)) # left barcodes minimun in eol and right barcodes max in eol
+		# blast file has duplicates of reads and we 
+		rownames(res) <- insertmatch$query 
+		res <- res[grepl("L[[:digit:]]+", res$left.bc) & grepl("R[[:digit:]]+", res$right.bc), ] # removing the nas
+		# list(
+		#   fractions = c(
+		#   	frac.compl = nrow(res) / nrow(bc_ident),
+		#   	frac.insert = nrow(insertmatch) / nrow(bc_ident),
+		#   	frac.bol = sum(!is.na(bc_ident$bol)) / nrow(bc_ident),
+		#   	frac.eol = sum(!is.na(bc_ident$eol)) / nrow(bc_ident)
+		#   ),
+		#   result.counts = table(res),
+		#   result = res)
+	})
+	names(results) <- names(files)
+	results
 }
